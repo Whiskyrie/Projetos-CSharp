23f76d610c860479b9f9a5e68e230c85bbb01e9d
Evandro Roberto Ropelatto FIlho <evandroropfilho@gmail.com>
2024-04-04 16:08:26 -0300

    2

--- 4 - Lista/1 - Atividade/obj/Debug/net8.0/1 - Atividade.AssemblyInfo.cs
+++ 4 - Lista/1 - Atividade/obj/Debug/net8.0/1 - Atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1 - Atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80e74737668d0a644e873ca164edfd67172e27f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0afcff5aea38aa499e62d508e1160019afd0f0dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("1 - Atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1 - Atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
